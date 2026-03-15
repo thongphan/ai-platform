7fa1b397f39ab0c264e586a2dfef9542b455966b
Phan Anh Thong <phananhthong@gmail.com>
2026-03-15 09:11:52 +0700

    update cloudflared url

--- app/app.py
+++ app/app.py
@@ -22,7 +22,7 @@ app.include_router(router)
 logger.info("FastAPI application started")
 if __name__ == '__main__':
     uvicorn.run(
-        "rag_practice_coursesa.fastapi:app",
+        "app:app",
         host="0.0.0.0",
         port=8000,
         reload=True
